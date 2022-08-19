7c449214f5ac292dc0f4daa9f96ec56aa422f860
Cass K.M. Lam <cass.lam@westcom.com.hk>
2022-08-19 18:06:49 +0800

    added collapse function

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,4 +1,12 @@
-var myDropdown = document.getElementById('myDropdown')
+var myDropdown = document.getElementById('myDropdown');
 myDropdown.addEventListener('show.bs.dropdown', function () {
   // do something...
+});
+
+var myCollapsible = document.getElementById('reply');
+var myCollapse = document.getElementById('replyCollapse');
+myCollapsible.addEventListener('show.bs.collapse', function () {
+  // do something...
+  // myCollapse.toggle();
+  // alert('hi');
 })
